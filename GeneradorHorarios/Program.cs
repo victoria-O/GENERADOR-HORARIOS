71769d719cff769cd51881e61b2a74379518308e
victoria-O <orellanamolle@gmail.com>
2019-09-29 20:39:58 -0400

    first commit

---
+++ GeneradorHorarios/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace GeneradorHorarios
+{
+    class Program
+    {
+        //itext
+        //evopdf
+        //github:public(clase)
+        //       private(proyect)
+        static void Main(string[] args)
+        {
+            
+            PdfReader leer = new PdfReader("C:\\Users/windws10/Desktop/TOPICOS 2/GeneradorHorarios/Horarios Sistemas II-2019.pdf");
+            string text = string.Empty;
+            for (int i = 1; i <= leer.NumberOfPages; i++)
+            {
+                text += PdfTextExtractor.GetTextFromPage(leer, i);
+            }
+            // Console.WriteLine(text);
+            string line = text.Split("\n");
+            for(int i= 1;i<=line.Length; i++)
+            {
+                
+                line += PdfTextExtractor.GetTextFromPage(leer, i);
+                
+            }
+            Console.WriteLine(line);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GeneradorHorarios/pdfParser.cs
@@ -0,0 +1,18 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneradorHorarios
+{
+    public class pdfParser
+    {
+        public int SisMat { get; set; }
+        public string NombreMateria { get; set; }
+        public int Grupo { get; set; }
+        public string Horario { get; set; }
+        public string NombreDocente { get; set; }
+    }
+}
